a3355c106cbb39ab04bee4fb24ddae3e4e40a1f1
GCA\jgonz296 <gonzojuan@hotmail.com>
2022-10-20 11:39:21 +0200

    Prueba inicial

---
+++ src/com/emc/reflection/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.emc.reflection;
+
+public class HelloWorld {
+	void sayHello() {
+		System.out.println("Hola mundo");
+	}
+	
+	void sayHello(String name) {
+		System.out.println("Hola "+name);
+	}
+
+}
